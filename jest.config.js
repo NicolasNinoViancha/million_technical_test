09284c045f22775dbaa733a112bfc8766cf58358
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-05-10 16:31:02 -0500

    test(jest): config test for react-navigation library

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   setupFiles: ['./__tests__/__mocks__/react-native-config.js'],
   testPathIgnorePatterns: ['/node_modules/'],
   testMatch: ['**/__tests__/**/*.test.tsx'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|@react-navigation)/)',
+  ],
 };
