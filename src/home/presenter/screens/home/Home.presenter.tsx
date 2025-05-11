fa5d5fbe8b47b11700f4de46c0ce75619afc6ba4
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-05-10 23:07:12 -0500

    feat(presenter): integrate useGetCryptoList hook for cryptocurrency data management

--- src/home/presenter/screens/home/Home.presenter.tsx
+++ src/home/presenter/screens/home/Home.presenter.tsx
@@ -1,8 +1,16 @@
 import HomeComponent from './Home.component';
+import {useGetCryptoList} from '@home/presenter/hooks';
 import {HomeScreenModels} from './models';
 import {NavigationModels} from '@shared/navigation/models';
 
 const HomePresenter = ({navigation}: HomeScreenModels.PropsPresenter) => {
+  const {
+    isLoadingGetCryptoList,
+    isLoadingMoreGetCryptoList,
+    isErrorGetCryptoList,
+    cryptoList,
+    getMoreCryptoList,
+  } = useGetCryptoList();
   return (
     <HomeComponent
       onShowDetails={cryptoID =>
