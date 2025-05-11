0e4236fc03aed079893facc0015824653b9ae11d
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-05-10 23:10:46 -0500

    feat(crypto): add CryptoAdapter for transforming CryptoDto to CryptoEntity

---
+++ src/home/infrastructure/adapters/crypto.adapter.ts
@@ -0,0 +1,27 @@
+import {CryptoEntity} from '@home/domain/entities';
+import {CryptoDto} from '../dto';
+
+export class CryptoAdapter {
+  static fromDto(dto: CryptoDto): CryptoEntity {
+    if (!dto.id) {
+      throw new Error('Error:Crypto data invalid');
+    }
+    return {
+      id: dto.id ?? '',
+      symbol: dto.symbol ?? '',
+      name: dto.name ?? '',
+      rank: dto.rank ?? 0,
+      priceUSD: dto.price_usd ?? '',
+      percentChange24h: dto.percent_change_24h ?? '',
+      percentChange1h: dto.percent_change_1h ?? '',
+      percentChange7d: dto.percent_change_7d ?? '',
+      priceBTC: dto.price_btc ?? '',
+      marketCapUSD: dto.market_cap_usd ?? '',
+      volume24: dto.volume24 ?? 0,
+      volume24a: dto.volume24a ?? 0,
+      csupply: dto.csupply ?? '',
+      tsupply: dto.tsupply ?? '',
+      msupply: dto.msupply ?? '',
+    };
+  }
+}
