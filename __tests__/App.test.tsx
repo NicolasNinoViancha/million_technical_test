8246ef94a0f22f95b7900b721fb4044a349dd1c2
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-05-10 23:11:44 -0500

    test(App): update test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,8 +6,14 @@ import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../src/App';
 
+jest.mock('@home/presenter/di', () => ({
+  homeUseCases: {
+    getCryptoList: jest.fn().mockResolvedValue([]),
+  },
+}));
+
 test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
+  await ReactTestRenderer.act(async () => {
     ReactTestRenderer.create(<App />);
   });
 });
