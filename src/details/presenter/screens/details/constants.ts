af8e6a0e0945030c3c4cb5187db306b84ca0a2f0
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2025-05-11 23:13:46 -0500

    feat(details): enhance DetailsComponent with loading and error handling

---
+++ src/details/presenter/screens/details/constants.ts
@@ -0,0 +1,3 @@
+export const LABELS = {
+  ERROR: 'Sorry, something went wrong, try again later',
+};
--- src/details/presenter/screens/details/styles.ts
+++ src/details/presenter/screens/details/styles.ts
@@ -1,9 +1,27 @@
 import {StyleSheet} from 'react-native';
+import {THEME} from '@shared/theme';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: THEME.COLORS.BLACK,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  ctnHeader: {
+    marginBottom: 10,
+  },
+  ctnDetails: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 20,
+    paddingVertical: 25,
+    paddingHorizontal: 20,
+    backgroundColor: THEME.COLORS.LIGHT_BLACK,
+  },
+  error: {
+    fontSize: THEME.FONTSIZES[20],
+    color: THEME.COLORS.RED,
   },
 });
